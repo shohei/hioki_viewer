352c42ac5fd857aeccb5ac3a8ad755b6786afc72
Shohei Aoki <shoaok@gmail.com>
2017-03-22 14:45:07 +0900

    add goji server

--- server.go
+++ server.go
@@ -1,50 +1,22 @@
 package main
 
 import (
+"./parser"
 "fmt"
-"github.com/PuerkitoBio/goquery"
-"github.com/k0kubun/pp"
+"net/http"
+"github.com/zenazn/goji"
+"github.com/zenazn/goji/web"
     // "reflect"
 )
 
-
-func GetPage(url string){
-	dom,_ := goquery.NewDocument(url)
-	dom.Find("table").Children().Each(func(i int, n *goquery.Selection){
-		switch i {
-		case 2:
-			n.Children().Each(func(j int, nn *goquery.Selection){
-				switch j {
-				case 0:
-					pp.Print(nn.Text())
-					fmt.Println()
-				case 1:
-					break
-				default:
-					nn.Children().Each(func(k int, nnn *goquery.Selection){
-						switch k {
-						case 0:
-							fmt.Print("SENSOR: ")
-							pp.Print(nnn.Text())
-						case 1:
-							pp.Print(nnn.Text())
-							fmt.Println()
-						case 3:
-							fmt.Print("SENSOR: ")
-							pp.Print(nnn.Text())
-						case 4:
-							pp.Print(nnn.Text())
-							fmt.Println()
-						}
-						})
-				}
-				})
-		}
-		})
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
 }
 
 func main(){
 	fmt.Println("server started.")
 	url := "http://192.168.100.210/REALDATA.HTM"
-	GetPage(url)
+	parser.GetPage(url)
+	goji.Get("/hello/:name", hello)
+	goji.Serve()
 }
