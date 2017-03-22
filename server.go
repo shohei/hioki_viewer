599cb1ef273e5345dda365ff3805ffbd5eb37b66
Shohei Aoki <shoaok@gmail.com>
2017-03-22 14:55:52 +0900

    add go routine

--- server.go
+++ server.go
@@ -15,8 +15,11 @@ func hello(c web.C, w http.ResponseWriter, r *http.Request) {
 
 func main(){
 	fmt.Println("server started.")
+	ch := make(chan int)
+
 	url := "http://192.168.100.210/REALDATA.HTM"
-	parser.GetPage(url)
+	go parser.ParsePeriodically(url,ch)
+
 	goji.Get("/hello/:name", hello)
 	goji.Serve()
 }
