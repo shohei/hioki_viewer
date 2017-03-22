6b3332dd214631f1d4582481f8ad66e5bdc49d6b
Shohei Aoki <shoaok@gmail.com>
2017-03-22 14:31:39 +0900

    parser implemented

--- server.go
+++ server.go
@@ -1,48 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "golang.org/x/net/html"
-    "github.com/PuerkitoBio/goquery"
+"fmt"
+"github.com/PuerkitoBio/goquery"
+"github.com/k0kubun/pp"
+    // "reflect"
 )
 
-func showData(index int, s *goquery.Selection){
-	count := 0
-	func (){
-		fmt.Println(count)
-		fmt.Println(s.Children())
-		// fmt.Println(s.Children()[0].Text())
-		// fmt.Println(s.Children()[1].Text())
-		// fmt.Println(s.Children()[3].Text())
-		// fmt.Println(s.Children()[4].Text())
-	}()
-    count++
-}
 
 func GetPage(url string){
 	dom,_ := goquery.NewDocument(url)
-	fmt.Println(dom.Find("table").Nodes).Each(func(_ int, n *html.Node){
-			fmt.Println(*n.Data)
-	})
-	// fmt.Println(dom.Find("table").Children().Text())
-	// fmt.Println(dom.Find("table").Next().Text())
-	// fmt.Println(dom.Find("table").Next().Next().Text())
-// 	dom.Find("table").Next().Next().firstChild.Each(func(index int, s *goquery.Selection){
-// 		switch(index){
-// 			case 0:
-// 				fmt.Println(s)
-// 				// s.FirstChild
-// 				fmt.Println("")
-// 			case 1:
-// 				return
-// 			default:
-// 				showData(index,s)
-// 		}
-// 	})
+	dom.Find("table").Children().Each(func(i int, n *goquery.Selection){
+		switch i {
+		case 2:
+			n.Children().Each(func(j int, nn *goquery.Selection){
+				switch j {
+				case 0:
+					pp.Print(nn.Text())
+					fmt.Println()
+				case 1:
+					break
+				default:
+					nn.Children().Each(func(k int, nnn *goquery.Selection){
+						switch k {
+						case 0:
+							fmt.Print("SENSOR: ")
+							pp.Print(nnn.Text())
+						case 1:
+							pp.Print(nnn.Text())
+							fmt.Println()
+						case 3:
+							fmt.Print("SENSOR: ")
+							pp.Print(nnn.Text())
+						case 4:
+							pp.Print(nnn.Text())
+							fmt.Println()
+						}
+						})
+				}
+				})
+		}
+		})
 }
 
 func main(){
-  fmt.Println("server started.")
-  url := "http://192.168.100.210/REALDATA.HTM"
-  GetPage(url)
+	fmt.Println("server started.")
+	url := "http://192.168.100.210/REALDATA.HTM"
+	GetPage(url)
 }
