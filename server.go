7beecb24604793c79310281cf6d560129723cee0
Shohei Aoki <shoaok@gmail.com>
2017-03-27 18:09:08 +0900

    implement template

--- server.go
+++ server.go
@@ -1,26 +1,42 @@
 package main
 
 import (
-// "github.com/k0kubun/pp"
-"./parser"
-"fmt"
-"net/http" 
-"encoding/json"
-"math/rand"
-"time"
-"flag"
-"os/exec"
-"regexp"
+	// "github.com/k0kubun/pp"
+	"./parser"
+	"fmt"
+	"net/http" 
+	"encoding/json"
+	"math/rand"
+	"time"
+	"flag"
+	"os/exec"
+	"regexp"
+	"text/template"
 )
 
 var host_ip *string
 var logger_ip *string
+
+type Page struct {
+	HostIp string 
+}
 
 type DataArray struct {
 	Number int
 	SensorData *parser.SensorData
 }
 
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	page := Page{HostIp: *host_ip}
+	tpl, err1 := template.ParseFiles("index.html")
+	if err1 != nil {
+		panic(err1)
+	}
+	err2 := tpl.Execute(w,page)
+	if err2 != nil {
+		panic(err2)
+	}
+}
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	url := "http://"+ *logger_ip +"/REALDATA.HTM"
 
@@ -32,7 +48,6 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 	hour := utc.Hour()
 	minute := utc.Minute()
 	second := utc.Second()
-	// fmt.Println(year,month,day,hour,minute,second)
 	datetime := []int {year,month,day,hour,minute,second}
 
 	ds := parser.Parse(url,datetime)
@@ -44,7 +59,6 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	bytes, _ := json.Marshal(das)
 
-	// fmt.Println("Received Request")
 	w.Header().Set("Content-Type", "text/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(bytes)
@@ -62,23 +76,24 @@ func main(){
 	outstr := string(out)
 	r := regexp.MustCompile("192\\.\\d*\\.\\d*\\.\\d")
 	res := r.FindAllString(outstr,-1)	
-	host_ip := res[0]
-	fmt.Println("host ip address is:",host_ip)
+	host_ip := &res[0]
+	fmt.Println("Host IP address:",*host_ip)
 
 	logger_ip = flag.String("logger-ip", "192.168.100.210", "IP address of hioki logger")
+	fmt.Println("Logger IP address:",*logger_ip)
 	fmt.Println("server started.")
 
 	staticServer := http.NewServeMux()
-	staticServer.Handle("/",http.FileServer(http.Dir("./")))
+	staticServer.HandleFunc("/",staticHandler)
 
 	logServer := http.NewServeMux()
 	logServer.HandleFunc("/",MainHandler)
 
 	go func(){
-		http.ListenAndServe(":8000",staticServer)
+		http.ListenAndServe(":3000", logServer)
 	}()
 	go func(){
-		http.ListenAndServe(":3000", logServer)
+		http.ListenAndServe(":8000",staticServer)
 	}()
 	<- finish
 }
